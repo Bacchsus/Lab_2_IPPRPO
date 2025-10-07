d29838dc99b08d00b5c23ca9bd83e1c06e76ca83
CTPweb-pr <example_mail@gmail.com>
2025-10-07 18:16:18 +0300

    Add class Factorial

---
+++ Lab_2/src/main/java/org/example/Factorial.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Factorial {
+    public static int Factorial(int number){
+        int result = 1;
+        for(int i = 1; i <= number; i++){
+            result *= i;
+        }
+        return result;
+    }
+}
