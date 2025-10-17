d11f8434a796c7fc644f9344d1d73fe0e08773d8
CTPweb-pr <example_mail@gmail.com>
2025-10-17 16:47:43 +0300

    modified:   Factorial/src/main/java/org/example/Main.java

--- Factorial/src/main/java/org/example/Main.java
+++ Factorial/src/main/java/org/example/Main.java
@@ -2,9 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        double PI = 3.1415926;
-        System.out.println("Hello, World!");
-        System.out.println(PI);
-        System.out.println("Feature");
+
     }
 }
