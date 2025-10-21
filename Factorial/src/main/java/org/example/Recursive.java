226a0a521e7fd142ae5e9145a316d113af35b5cc
CTPweb-pr <example_mail@gmail.com>
2025-10-21 14:39:23 +0300

    new file:   src/main/java/org/example/Recursive.java

---
+++ Factorial/src/main/java/org/example/Recursive.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Recursive {
+
+    public long recursive(int number) {
+        if(number <= 1) {
+            return 1;
+        }
+        else {
+            return number * recursive(number--);
+        }
+    }
+}
