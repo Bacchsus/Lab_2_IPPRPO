f9dccb631c7ab5c710e763a99b6ceeffd08c4718
CTPweb-pr <example_mail@gmail.com>
2025-10-21 14:30:21 +0300

    new file:   src/main/java/org/example/Factorial.java 	modified:   src/main/java/org/example/Main.java

---
+++ Factorial/src/main/java/org/example/Factorial.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Factorial {
+
+    public long Iterative(int number) {
+        long result = 1;
+        for(int i = 2; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
--- Factorial/src/main/java/org/example/Main.java
+++ Factorial/src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
 
 public class Main {
+
+    long Resursive(int number) {
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
